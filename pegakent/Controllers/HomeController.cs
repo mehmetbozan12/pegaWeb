9688e7f67722d04d1f0d2dbb97d81528ebb1008c
mehmet bozan <mehmetbzn03@gmail.com>
2025-06-28 15:49:40 +0300

    inital

---
+++ pegakent/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pegakent.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult hakkimizda()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult iletisim()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult devamedenprojeler()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult tamamlananprojeler()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult teklifal()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult grupsirketlerimiz()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult baslanacakprojeler()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ pegakent/Controllers/devamedenprojelerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pegakent.Controllers
+{
+    public class devamedenprojelerController : Controller
+    {
+        // GET: devamedenprojeler
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult pegalifekonutlari()
+        {
+            return View();
+        }
+
+        public ActionResult pegapremiumkonutlari()
+        {
+            return View();
+        }
+
+        public ActionResult pegaloftkonutlari()
+        {
+            return View();
+        }
+    }
+}
